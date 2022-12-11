01571d08fb5b1051510e5f26bd2e3df8d3ece7f2
Arnaud293 <arnaudlt293@gmail.com>
2022-12-11 16:35:31 +0100

    fetch a random trending

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Nav from './Nav';
+import requests from '../config/Req';
+import axios from 'axios';
 
 const Header = () => {
-    return (
-        <>
-        <Nav />
-        <div className='header'>
+
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        const getData = async () => {
+            const request = await axios.get(requests.getTrending)
             
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            )
+        }
+        getData();
+    }, []);
+    console.log(movie)
+
+    return (
+        <div className='header'>
+            <Nav />
         </div>
-        </>
     );
 };
 
