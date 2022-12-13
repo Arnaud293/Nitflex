0aa6638c03133890918143868e1d14b00d96b7cf
Arnaud293 <arnaudlt293@gmail.com>
2022-12-13 12:04:48 +0100

    pass data from mapped card to clicked card for quickview, preparing style

--- src/components/QuickView.js
+++ src/components/QuickView.js
@@ -1,9 +1,23 @@
 import React from 'react';
 
-const QuickView = () => {
+const QuickView = ({program}) => {
     return (
         <div className='quick-view'>
-            
+            <div className="quick-view-container">
+                <div className="quick-view-container-img">
+                    <img src={`https://image.tmdb.org/t/p/original/${program.backdrop_path}`} alt={program?.title || program?.original_title} />
+                    <h1>{program.title}</h1>
+                    <div className="quick-view-btn">
+                        <button></button>
+                        <button></button>
+                        <button></button>
+                    </div>
+                </div>
+                <div className="quick-view-container-infos">
+
+                </div>
+
+            </div>
         </div>
     );
 }; 
--- src/components/Row.js
+++ src/components/Row.js
@@ -1,10 +1,12 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import QuickView from './QuickView';
 
 const Row = ({fetchUrl, title}) => {
     const [programs, setPrograms] = useState([]);
     const [popup, setPopup] = useState(false);
-    console.log(title);
+    const [popupData, setPopupData] = useState([]);
+    
 
     useEffect(() => {
         const getData = async () => {
@@ -16,7 +18,20 @@ const Row = ({fetchUrl, title}) => {
         }
         getData();
     }, []);
+
+    const handlePopUp = (e) => {
+        popup ? setPopup(false) : setPopup(true);
+        // console.log(e.target.src)
+        // console.log(e.target.src.split('https://image.tmdb.org/t/p/original/').join(""))
+        const result = programs.filter(movie => movie.backdrop_path === e.target.src.split('https://image.tmdb.org/t/p/original/').join(""));
+        console.log(result);
+        setPopupData(result);
+    }
+
     return (
+        <>
+        {popup && <QuickView program={popupData[0]}/>}
+                
         <div className="top-ten-container">
             <h2>{title}</h2>
             {
@@ -25,18 +40,23 @@ const Row = ({fetchUrl, title}) => {
             .sort((a,b) => b.vote_average - a.vote_average)
             .slice(0,10)
             .map((program, index) => (
-                <div className='top-ten-card'>
+                
+                <div className='top-ten-card' key={program.id} onClick={(e) => handlePopUp(e)}>
                     <img src={`https://image.tmdb.org/t/p/original/${program.backdrop_path}`} alt={program?.title || program?.original_title} />
+                    
                 </div>
+              
+                
             ))
             ) :(
                 programs.map((program, index) => (
-                    <div className='top-ten-card'>
+                    <div className='top-ten-card' key={index}>
                         <img src={`https://image.tmdb.org/t/p/original/${program.backdrop_path}`} alt={program?.title || program?.original_title} />
                     </div>
                 )) 
             )}
         </div>
+        </>
     );
 };
 
