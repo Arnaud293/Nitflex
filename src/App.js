966aff92ddf4c3fa88919da193fb0f6ee4a03fdb
Arnaud293 <arnaudlt293@gmail.com>
2022-12-15 16:14:46 +0100

    render with conditions when someone search with searchbar= display custom

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Movies from './pages/Movies';
@@ -6,20 +6,26 @@ import TvShows from './pages/TvShows';
 import Favorites from './pages/Favorites';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
+import SearchResult from './components/SearchResult';
 
 const App = () => {
-  
+  const [getSearchResult, setGetSearchResult] = useState('');
+  console.log(getSearchResult);
+  // getSearchResult.length &&
   return (
     <div>
         
         <BrowserRouter>
-          <Nav />
+          <Nav result={getSearchResult} setResult={setGetSearchResult} />
+          {getSearchResult.length > 0 ? (
+            <SearchResult result={getSearchResult} />
+          ) :(
           <Routes>
             <Route path='/' element={<Home />}/>
             <Route path='/movies' element={<Movies />}/>
             <Route path='/tvshows' element={<TvShows />}/>
             <Route path='/favorites' element={<Favorites />}/>
-          </Routes>
+          </Routes>)}
           <Footer />
         </BrowserRouter>
         
