38fd7b66d1086b7ad6878959b8ccbfa93628f612
Arnaud293 <arnaudlt293@gmail.com>
2022-12-15 13:58:43 +0100

    add nav and footer at the top of app

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import Home from './pages/Home';
 import Movies from './pages/Movies';
 import TvShows from './pages/TvShows';
 import Favorites from './pages/Favorites';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 const App = () => {
   
   return (
     <div>
+        
         <BrowserRouter>
+          <Nav />
           <Routes>
             <Route path='/' element={<Home />}/>
             <Route path='/movies' element={<Movies />}/>
             <Route path='/tvshows' element={<TvShows />}/>
             <Route path='/favorites' element={<Favorites />}/>
           </Routes>
+          <Footer />
         </BrowserRouter>
         
     </div>
