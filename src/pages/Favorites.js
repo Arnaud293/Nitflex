4f2419efd15d7b9e06b46809c59ec36c8137a990
Arnaud293 <arnaudlt293@gmail.com>
2022-12-16 10:10:28 +0100

    get and display from storage

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,11 +1,55 @@
-import React from 'react';
-import Footer from '../components/Footer';
-import Nav from '../components/Nav';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import requests from '../config/Req';
 
 const Favorites = () => {
-    return (
-        <div>
+    const [favoritesMovies, setFavoritesMovies] = useState([]);
+    const [favoritesTv, setFavoritesTv] = useState([]);
 
+    // Get stored movies
+    useEffect(() => {
+        let moviesArr = [];
+        let moviesId = window.localStorage.movies ? window.localStorage.movies.split(',') : [];
+        for (let i=0; i < moviesId.length; i++){
+            axios.get(`https://api.themoviedb.org/3/movie/${moviesId[i]}?api_key=ed82f4c18f2964e75117c2dc65e2161d`)
+            .then(res => moviesArr.push(res.data))
+            .then(() => setFavoritesMovies(moviesArr));     
+        }
+    },[])
+    // Get stored Tv shows
+    useEffect(() => {
+        let tvArr = [];
+        let tvId = window.localStorage.tv ? window.localStorage.tv.split(',') : [];
+        for (let i=0; i < tvId.length; i++){
+            axios.get(`https://api.themoviedb.org/3/tv/${tvId[i]}?api_key=ed82f4c18f2964e75117c2dc65e2161d`)
+            .then(res => tvArr.push(res.data))
+            .then(() => setFavoritesTv(tvArr));    
+        }
+    },[])
+    console.log(favoritesMovies)
+    console.log(favoritesTv)
+    return (
+        <div className='favorites'>
+            <div className="favorites-movies">
+                <h2>My favorites movies :</h2>
+                <div className="favorites-cards-container">
+                {favoritesMovies.map((movie) => (
+                    <div className='favorites-card' key={movie.id}>
+                        <img src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} />
+                    </div>
+                ))}
+                </div>
+            </div>
+            <div className="favorites-tv">
+                <h2>My favorites tv shows :</h2>
+                <div className="favorites-cards-container">
+                {favoritesTv.map((tv) => (
+                    <div className='favorites-card' key={tv.id}>
+                        <img src={`https://image.tmdb.org/t/p/original/${tv.backdrop_path}`} />
+                    </div>
+                ))}
+                </div>
+            </div>
         </div>
     );
 };
